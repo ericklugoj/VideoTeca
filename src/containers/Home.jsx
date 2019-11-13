8f016abebbd7b3e2d7d9f54616cdad941fecbc0f
Erick Lugo <ericklugoj@gmail.com>
2019-11-12 21:25:05 -0600

    Rutas terminadas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Header from '../components/Header';
-import Search from '../components/Search';
-import Categories from '../components/Categories';
-import Carousel from '../components/Carousel';
-import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import Footer from "../components/Footer";
 
-import useInitialState from '../hooks/useInitialState';
+import useInitialState from "../hooks/useInitialState";
 
-import '../assets/styles/App.scss';
+import "../assets/styles/App.scss";
 
-const API = 'http://localhost:3000/initialState';
+const API = "http://localhost:3000/initialState";
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return (
-    <div className="App">
-      <Header />
+    <>
       <Search />
 
       {initialState.mylist.length > 0 && (
@@ -46,10 +45,8 @@ const App = () => {
           ))}
         </Carousel>
       </Categories>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
-export default App;
+export default Home;
---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => (
+  <>
+    <h1>No Encontrado</h1>
+    <h2>Regresa al home</h2>
+  </>
+);
+
+export default NotFound;
