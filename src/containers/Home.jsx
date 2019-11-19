987c0211a175dd3eea857f894ada920b3e9329f7
Erick Lugo <ericklugoj@gmail.com>
2019-11-18 22:56:43 -0600

    Empezando a trabajar en el login

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 import { connect } from "react-redux";
-import useInitialState from "../hooks/useInitialState";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
@@ -18,7 +17,11 @@ const Home = ({ myList, trends, originals }) => {
         <Categories title="Mi lista">
           <Carousel>
             {myList.map(item => (
-              <CarouselItem key={item.id} {...item} />
+              <CarouselItem
+                key={item.id}
+                {...item}
+                isList
+              />
             ))}
           </Carousel>
         </Categories>
